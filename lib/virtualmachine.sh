95598e4bfd44d8c468541231651cd496416d7375
Luke G. Lussenden <luke.lussenden@redhat.com>
2025-09-25 12:12:32 -0400

    testing

---
+++ lib/virtualmachine.sh
@@ -0,0 +1,20 @@
+function assert_single_pvc_volume {
+  NUM_VOLUMES=$(yq '.spec.template.spec.volumes | length' ${1})
+  if [ "$NUM_VOLUMES" == "1" ]; then echo "single PVC volume configuration detected"; else
+    echo "unsupported case with multiple volumes. "
+    exit -1
+  fi
+}
+
+function assert_dv_plus_cloud_init {
+	ASSERT_DV=$(yq '.spec.template.spec.volumes[] | select(.dataVolume) | has("name")' $1)
+	ASSERT_CINC=$(yq '.spec.template.spec.volumes[] | select(.cloudInitNoCloud) | has("userData")' $1)
+	ASSERT_NV_2=$(yq '.spec.template.spec.volumes | length' ${1})
+	if [ "${ASSERT_DV}" == "true" && "${ASSERT_CINC}" == "true" && "${ASSERT_NV_2}" == "2" ]; then
+	  echo "known configuration"
+	else
+	  echo Not of standard Datavolume+CloudInitNoCloud configuration
+	  exit -1
+	fi
+}
+
--- transform.sh
+++ transform.sh
@@ -1,28 +1,43 @@
 export VM_NAME=$1
-export SOURCE_NAMESPACE=openshift-mtv
-export TARGET_NAMESPACE=my-namespace
+export SOURCE_NAMESPACE=$2
+export TARGET_NAMESPACE=$3
+source lib/virtualmachine.sh
 
 # Get the existing Virtual Machine configuration
-#oc get virtualmachine $VM_NAME -n $SOURCE_NAMESPACE -o yaml > ${VM_NAME}.yaml
+oc get virtualmachine $VM_NAME -n $SOURCE_NAMESPACE -o yaml > ${VM_NAME}.yaml
 
-# Ensure the machine only has one Volume as this script is currently written with that assumption
-NUM_VOLUMES=$(yq '.spec.template.spec.volumes | length' ${VM_NAME}.yaml)
-if [ "$NUM_VOLUMES" == "1" ]; then echo OK; else 
-  echo "unsupported case with multiple volumes. will have to "
-  exit -1  
+if `assert_single_pvc_volume ${VM_NAME}.yaml`; then 
+    export PVC=$(yq '.spec.template.spec.volumes[0].persistentVolumeClaim.claimName' ${VM_NAME}.yaml)
+elif `assert_dv_plus_cloud_init  ${VM_NAME}.yaml`; then
+    DV_NAME=$(yq '.spec.template.spec.volumes[] | select(.dataVolume).dataVolume.name)' ${VM_NAME}.yaml) 
+    export PVC=$(oc get datavolume $DV_NAME -n $SOURCE_NAMESPACE -o yaml | yq '.status.claimName')
+else 
+  exit -1
 fi
 
-# get the Persistent Volume Claim (PVC) name
-export PVC=$(yq '.spec.template.spec.volumes[0].persistentVolumeClaim.claimName' ${VM_NAME}.yaml)
+export PVC_STORAGE=$(oc get pvc $PVC -n $SOURCE_NAMESPACE -o yaml | yq '.status.capacity.storage')
 
+mkdir -p new_vm_config
+
+# get the Persistent Volume Claim (PVC) name
+
+export PVC_STORAGE=30Gi
+export DV_CLONE="${VM_NAME}-${SOURCE_NAMESPACE}-clone"
 # Transform source configuration to destination configuration
 yq 'del(.status) | 
     del(.metadata) | 
     del(.. | select(has("macAddress")).macAddress) |
-    .metadata = { "namespace": strenv(TARGET_NAMESPACE), "name": strenv(VM_NAME) }
-   ' ${VM_NAME}.yaml > new-${VM_NAME}.yaml
+    .metadata = { "namespace": strenv(TARGET_NAMESPACE), "name": strenv(VM_NAME) } |
+    .spec.dataVolumeTemplates = {
+        "metadata":{"name": strenv(DV_CLONE)},
+        "spec": {
+                "storage": { 
+                    "accessModes": [ ReadWriteOnce ], 
+                    "resources": { "requests": { "storage": strenv(PVC_STORAGE) }
+                },
+                "source": {"pvc": {"namespace": strenv(SOURCE_NAMESPACE), "name":strenv(PVC) }}
+        }
+    }
+   ' ${VM_NAME}.yaml > new_vm_config/new-${VM_NAME}.yaml
 
-
-
-
-
+ 
